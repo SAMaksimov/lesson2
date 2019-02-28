198ab2e785881c8ac50e8ab61573a4bd342c407b
SAMaksimov <stepanmaksimov@gmail.com>
2019-02-28 22:41:30 +0300

    data transfer from file variables.js to money.js, deleting variables.js

--- money.js
+++ money.js
@@ -1,5 +1,25 @@
 'use strict';
-
+let startBtn                = document.getElementById('start'),
+    budgetValue             = document.getElementsByClassName('budget-value')[0],
+    daybudgetValue          = document.getElementsByClassName('daybudget-value')[0],
+    levelValue              = document.getElementsByClassName('level-value')[0],
+    expensesValue           = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue   = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue             = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue       = document.getElementsByClassName('monthsavings-value')[0],
+    YearSavingsValue        = document.getElementsByClassName('yearsavings-value')[0],
+    expensesItem            = document.getElementsByClassName('expenses-item'),
+    expensesItemBtn         = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn     = document.getElementsByTagName('button')[1],
+    countBudgetBtn          = document.getElementsByTagName('button')[2],
+    income                  = document.querySelector('#income'),
+    savings                 = document.querySelector('#savings'),
+    sum                     = document.querySelector('#sum'),
+    percent                 = document.querySelector('#percent'),
+    year                    = document.querySelector('.year-value'),
+    month                   = document.querySelector('.month-value'),
+    day                     = document.querySelector('.day-value');
+    
 let money, time;
 
 function startAll () {
